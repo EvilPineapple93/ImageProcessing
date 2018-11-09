e468fc6a5f68e3cd819589b9182e21af66346252
EvilPineapple93 <hmarceau@laurentian.ca>
2018-11-09 18:30:24 -0500

    Reads strokes from INKML file into list of strokes and plot ME

--- segmentation.py
+++ segmentation.py
@@ -1,8 +1,38 @@
 import xml.etree.ElementTree as ET
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def findXtrema(strokeList):
+    xmin = min(np.amin(stroke[:,0]) for stroke in strokeList)
+    xmax = max(np.amax(stroke[:,0]) for stroke in strokeList)
+    ymin = min(np.amin(stroke[:,1]) for stroke in strokeList)
+    ymax = max(np.amax(stroke[:,1]) for stroke in strokeList)
+    return [xmin, xmax],[-ymax,-ymin]
+
+def plotME(strokeList):
+    for stroke in strokeList:
+        plt.plot(stroke[:,0],-stroke[:,1], 'k')
+    xlims,ylims = findXtrema(strokeList)
+    plt.axes().set_aspect('equal')
+    plt.show()
+
 
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
-for child in root:
-    print(child.tag, child.attrib)
+pre  = (root.tag).rstrip('ink')
+print(pre)
+strokeList = []
+for stroke in root.findall(pre+'trace'):
+        print(stroke.get('id'), stroke.text)
+        coords = stroke.text.split(', ');
+        npCoords = []
+        for element in coords:
+            x,y = element.split(' ')
+            npCoords.append([float(x), float(y)])
 
+        npCoords = np.asarray(npCoords)
+        strokeList.append(npCoords)
+
+print(strokeList)
+plotME(strokeList)

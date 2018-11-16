3a400cf9b84936e0d29a9b44d416869e9016c640
EvilPineapple93 <hmarceau@laurentian.ca>
2018-11-15 20:23:51 -0500

    color coded stroke groups after preliminary

--- segmentation.py
+++ segmentation.py
@@ -6,8 +6,17 @@ import matplotlib.pyplot as plt
 
 # Plots mathematical expression, given list of strokes 
 def plotME(strokeList):
-    for stroke in strokeList:
-        plt.plot(stroke[:,0],-stroke[:,1], 'k')
+    c = 'k'
+    plt.plot(strokeList[0][:,0], -strokeList[0][:,1], c)
+    for i in range(1, len(strokeList)):
+        s1 = strokeList[i-1]
+        s2 = strokeList[i]
+        if strokeU(s1,s2) < 0.5:
+            if c == 'k':
+                c = 'b'
+            else:
+                c = 'k'
+        plt.plot(s2[:,0],-s2[:,1], c)
     plt.axes().set_aspect('equal')
     plt.show()
 
@@ -51,7 +60,6 @@ def startDist(stroke1, stroke2):
 # Backward movement between the ending point of stroke1 and the starting point
 # of stroke2
 def endDist(stroke1, stroke2):
-    print(stroke1[-1][0], stroke2[0][0])
     if stroke1[-1][0] > stroke2[0][0]:
         return sp.distance.pdist([stroke1[-1], stroke2[0]], 'euclidean')
     else:
@@ -70,6 +78,31 @@ def avgSLength(strokeList):
     for s in strokeList:
         l = l + length(s)
     return l/len(strokeList)
+
+# Stroke Unity percentage between s1 and s2
+def strokeU(s1, s2):
+    md = minDist(s1, s2)
+    ov = overlap(s1, s2)
+    sd = startDist(s1, s2)
+    ed = endDist(s1, s2)
+    mdP = max(1-md/avgL, 0)
+    sdP = max(1-sd/avgL, 0)
+    edP = min(ed/avgL, 1.0)
+    a = 4
+    b = 3
+    c = 2
+    d = 1
+    if (mdP < sdP and mdP < edP and mdp < ov):
+        a = 0
+    elif (ov < mdP and ov < sdP and ov < edP):
+        b = 0
+    elif (sdP < mdP and sdP < ov and sdP < edP):
+        d = 0
+    elif (edP < mdP and edP < ov and edP < sdP):
+        c = 0
+    strokeUnity = (a*mdP + b*ov + c*edP + d*sdP)/(a+b+c+d)
+    print(strokeUnity)
+    return strokeUnity
 
 # Determine xml prefix to be removed when parsing data
 tree = ET.parse(sys.argv[1])
@@ -94,19 +127,6 @@ print(avgL)
 for i in range(1, len(strokeList)):
     s1 = strokeList[i-1]
     s2 = strokeList[i]
-    md = minDist(s1, s2)
-    ov = overlap(s1, s2)
-    sd = startDist(s1, s2)
-    ed = endDist(s1, s2)
-    mdP = max(1-md/avgL, 0)
-    sdP = max(1-sd/avgL, 0)
-    edP = min(ed/avgL, 1.0)
-    # print('Min dist: ', md, 'prob: ', mdP )
-    # print('Overlap: ', ov, 'prob: ', ov)
-    # print('Start dist: ', sd, 'prob: ', sdP)
-    # print('End dist: ', ed, 'prob: ', edP)
-    strokeUnity = (mdP + sdP + ov + edP)/4
-    print('Stroke Unity: ', strokeUnity)
 input('Display? (y/n)')
 
 plotME(strokeList)

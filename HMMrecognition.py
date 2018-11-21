90af1d3d0321494d9aa4a71f0f2672a901f552c0
Collin Marando <collin.marando@hotmail.com>
2018-11-21 15:09:33 -0500

    Final cleanup, ready to work on HMM

--- HMMrecognition.py
+++ HMMrecognition.py
@@ -4,24 +4,13 @@ import scipy.spatial as sp
 import matplotlib.pyplot as plt
 from hmmlearn import hmm
 
-    
-
 strokeList = np.load("strokeList.npy")
-print(strokeList)
-
-strokeList = np.load("unityList.npy")
-print(strokeList)
-
-
-
-
-
+unityList = np.load("unityList.npy")
 
 
 
 X = np.concatenate([observations, results])
 lengths = [len(obserations), len(results)]
-
 
 model = hmm.GaussianHMM(n_components=50, covariance_type="full", n_iter=100)
 model.fit(X)

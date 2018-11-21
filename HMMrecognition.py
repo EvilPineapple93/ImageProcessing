12ce4d519fcdb9ca1546eca15ec807d7af64ec88
Paul Tonon <paultonon@Pauls-MacBook-Pro.local>
2018-11-21 15:04:02 -0500

    HMM stuff

--- HMMrecognition.py
+++ HMMrecognition.py
@@ -3,12 +3,16 @@ import numpy as np
 import scipy.spatial as sp 
 import matplotlib.pyplot as plt
 from hmmlearn import hmm
-f = np.array([])
-file = open(“segmentExportData.txt”, “r”) 
-for e in file:
-    print np.append(file.readline())
 
     
+
+
+
+
+
+
+
+
 
 
 X = np.concatenate([observations, results])

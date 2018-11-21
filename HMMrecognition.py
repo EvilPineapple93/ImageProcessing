a99389a040419f2f63f9592dd6d3ad0dadb4b78a
Paul Tonon <paultonon@Pauls-MacBook-Pro.local>
2018-11-21 14:54:52 -0500

    HMM stuff

--- HMMrecognition.py
+++ HMMrecognition.py
@@ -2,4 +2,23 @@ import sys
 import numpy as np
 import scipy.spatial as sp 
 import matplotlib.pyplot as plt
+from hmmlearn import hmm
+f = np.array([])
+file = open(“segmentExportData.txt”, “r”) 
+for e in file:
+    print np.append(file.readline())
+
+    
+
+
+X = np.concatenate([observations, results])
+lengths = [len(obserations), len(results)]
+
+
+model = hmm.GaussianHMM(n_components=50, covariance_type="full", n_iter=100)
+model.fit(X)
+
+
+
+
 

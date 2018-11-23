093d1f7e2fc4a2276ac7b84f453c3ed15d25c916
Paul Tonon <paultonon@Pauls-MacBook-Pro.local>
2018-11-23 17:08:27 -0500

    Some more markov stuff

--- testingHMM.py
+++ testingHMM.py
@@ -13,7 +13,7 @@ from sklearn.externals import joblib
 ZeroM = joblib.load("ZeroMarkov.pkl") 
 OneM =  joblib.load("OneMarkov.pkl") 
 
-Testing = np.array([])
+Testing = np.empty((1,32**2))
 dir_path = os.path.dirname(os.path.realpath('Testing/train_30_30148.png'))
 n =0
 for filename in os.listdir('Testing/'):
@@ -25,14 +25,14 @@ for filename in os.listdir('Testing/'):
     wpercent = (basewidth/float(image.size[0]))
     hsize = int((float(image.size[1])*float(wpercent)))
     image = image.resize((basewidth,hsize), Image.ANTIALIAS)
-    image = np.array(image)
-    image = image.reshape(-1)
-    Testing = np.concatenate((Testing,image), axis=0)
+    image = np.array(image).reshape((1,32**2))
+    #image = image.reshape(-1)
+    Testing = np.vstack((Testing,image))
     n= n+1
 
-Testing = Testing.reshape(basewidth**2,n)  
-
-
-print ZeroM.score(Testing.T)
-print OneM.score(Testing.T)
+#Testing = Testing.reshape(basewidth**2,n)  
+Testing = np.delete(Testing,0,0)
+print Testing.shape
+print ZeroM.score(Testing[2].reshape(1,-1))
+print OneM.score(Testing[2].reshape(1,-1))
 
